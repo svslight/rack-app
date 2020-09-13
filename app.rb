68d12b34222d0a97fdf336c20b9f275ebec39d7c
Svetlana Solodovnikova <svslight@yandex.ru>
2020-09-13 19:32:47 +0500

    Task 19. Rack. TimeService

--- app.rb
+++ app.rb
@@ -1,25 +1,32 @@
-class App
-    def call(env)
-      # p self.class
-      perform_request
-      [status, headers, body]
-    end  
-    
-    private
+require_relative 'time_service' 
 
-    def perform_request
-      sleep rand(2..3)
+class App
+
+  def call(env)
+    @request = Rack::Request.new(env)
+    @response = Rack::Response.new
+
+    if @request.path_info == "/time"
+      time_response(TimeService.new(@request.params["format"]))
+    else
+      compose_response(404, "Unknown resource '#{@request.path_info}'\n")
     end
-    
-    def status
-      200
-    end
-    
-    def headers
-      { 'Content-Type' => 'text/plain' }
-    end
-    
-    def body
-      [ "Welcome aboard!\n"]
-    end    
   end
+
+  private
+
+  def compose_response(status, text)
+    @response.status = status
+    @response.write "#{text}"
+    @response.headers['Content-Type'] = 'text/plain'
+    @response.finish
+  end
+
+  def time_response(handler)
+    if handler.unknown_formats.empty?
+      compose_response(200, "#{handler.result}\n")
+    else
+      compose_response(400, "Unknown time format #{handler.unknown_formats}\n")
+    end
+  end
+end
